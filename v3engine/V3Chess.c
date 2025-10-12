59acb437e7413ad9ac25631f74494d5f664432ad
Sarge Rogatch <serge.rogatch@gmail.com>
2025-10-12 23:09:37 +0200

    Generalize from king-attacked to cell-attacked

--- v3engine/V3Chess.c
+++ v3engine/V3Chess.c
@@ -89,10 +89,9 @@ bool IsOnBoad(int8_t row, int8_t col)
   return row >= 0 && row < 8 && col >= 0 && col < 8;
 }
 
-CheckState GetCheckState(const ChessGameState *cgs, bool whiteKing) {
-  const int8_t kingRow = (whiteKing ? cgs->whiteKingRow_ : cgs->blackKingRow_);
-  const int8_t kingCol = (whiteKing ? cgs->whiteKingCol_ : cgs->blackKingCol_);
-
+// Returns check state if a king is placed at the specified row and column, e.g. because it stands there in the game,
+// or because it transitions the cell during castling.
+CheckState IsAttacked(const ChessGameState *cgs, const uint8_t kingRow, const uint8_t kingCol, const bool whiteKing) {
   // Search for opponent's knight making the check
   for (int8_t i=0; i<8; i++)
   {
@@ -113,8 +112,8 @@ CheckState GetCheckState(const ChessGameState *cgs, bool whiteKing) {
   // Check attacks by opponent's pawns
   for (int8_t dc=-1; dc<=+1; dc+=2)
   {
-    const int8_t oppPawnRow = (whiteKing ? cgs->whiteKingRow_+1 : cgs->blackKingRow_-1);
-    const int8_t oppPawnCol = (whiteKing ? cgs->whiteKingCol_+dc : cgs->blackKingCol_+dc);
+    const int8_t oppPawnRow = (whiteKing ? kingRow+1 : kingRow-1);
+    const int8_t oppPawnCol = kingCol+dc;
     if (!IsOnBoad(oppPawnRow, oppPawnCol)) {
       continue;
     }
@@ -130,8 +129,8 @@ CheckState GetCheckState(const ChessGameState *cgs, bool whiteKing) {
   {
     for (int8_t k=1; k<=7; k++)
     {
-      const int8_t oppRookRow = (whiteKing ? cgs->whiteKingRow_ : cgs->blackKingRow_) + k * cRookDirs[iDir][0];
-      const int8_t oppRookCol = (whiteKing ? cgs->whiteKingCol_ : cgs->blackKingCol_) + k * cRookDirs[iDir][1];
+      const int8_t oppRookRow = kingRow + k * cRookDirs[iDir][0];
+      const int8_t oppRookCol = kingCol + k * cRookDirs[iDir][1];
       if (!IsOnBoad(oppRookRow, oppRookCol))
       {
         break;
@@ -155,8 +154,8 @@ CheckState GetCheckState(const ChessGameState *cgs, bool whiteKing) {
   {
     for (int8_t k=1; k<=7; k++)
     {
-      const int8_t oppRookRow = (whiteKing ? cgs->whiteKingRow_ : cgs->blackKingRow_) + k * cBishopDirs[iDir][0];
-      const int8_t oppRookCol = (whiteKing ? cgs->whiteKingCol_ : cgs->blackKingCol_) + k * cBishopDirs[iDir][1];
+      const int8_t oppRookRow = kingRow + k * cBishopDirs[iDir][0];
+      const int8_t oppRookCol = kingCol + k * cBishopDirs[iDir][1];
       if (!IsOnBoad(oppRookRow, oppRookCol))
       {
         break;
@@ -176,4 +175,10 @@ CheckState GetCheckState(const ChessGameState *cgs, bool whiteKing) {
   }
 
   return MakeCheckState(nAttackers > 0, nAttackers >= 2);
+}
+
+CheckState GetCheckState(const ChessGameState *cgs, bool whiteKing)
+{
+  return IsAttacked(cgs, whiteKing ? cgs->whiteKingRow_ : cgs->blackKingRow_,
+    whiteKing ? cgs->whiteKingCol_ : cgs->blackKingCol_, whiteKing);
 }
